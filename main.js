892f3a15a5806f49b16dbe08a87801aa72fbc7aa
vamshi <158734118+vamshikiranreddy@users.noreply.github.com>
2024-05-01 17:14:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text",{
+    strings:["Student","Full Stack Devoleper","VLSI Design Engineer","Learner" ],
+    typeSpeed : 10,
+    backSpeed : 100,
+    backDelay : 1000,
+    loop : true
+});
