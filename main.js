6f1ce9ce8e90b64a2f66a74594ac7fce3a84146f
vamshi <158734118+vamshikiranreddy@users.noreply.github.com>
2024-05-01 17:12:55 +0530

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-var typed = new Typed(".text",{
-    strings:["Student","Full Stack Devoleper","VLSI Design Engineer","Learner" ],
-    typeSpeed : 10,
-    backSpeed : 100,
-    backDelay : 1000,
-    loop : true
-});
